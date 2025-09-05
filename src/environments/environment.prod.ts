93598dd3e6dc07ecb697368bd917fec359df3259
arr331 <adrianrr1331@gmail.com>
2025-09-04 23:51:54 -0500

    hosting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyA9QEVpfDMGn44bPgtkXxgEquE2H2yoUFg",
+    authDomain: "inventario-pq.firebaseapp.com",
+    projectId: "inventario-pq",
+    storageBucket: "inventario-pq.firebasestorage.app",
+    messagingSenderId: "189780153964",
+    appId: "1:189780153964:web:0dccae04eab4365959ea49"
+  }
 };
